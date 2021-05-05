ccfc1c7ab2238e754e1cb8824473cb4906670161
n3ir <49719046+n3ir@users.noreply.github.com>
2021-05-05 14:02:26 +0200

    Added a custom color selector

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -2,6 +2,7 @@ import "./Content.css";
 import { useState } from "react";
 import Card from "./Card";
 import Copy from "./Copy";
+import CustomColorInput from './CustomColorInput';
 
 const Content = () => {
   const [background, setBackground] = useState("#F1F1F1");
@@ -60,6 +61,12 @@ const Content = () => {
           <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5 20l-1.359-2.038c-1.061.653-2.305 1.038-3.641 1.038-3.859 0-7-3.14-7-7h2c0 2.757 2.243 5 5 5 .927 0 1.786-.264 2.527-.708l-1.527-2.292h5.719l-1.719 6zm0-8c0-2.757-2.243-5-5-5-.927 0-1.786.264-2.527.708l1.527 2.292h-5.719l1.719-6 1.359 2.038c1.061-.653 2.305-1.038 3.641-1.038 3.859 0 7 3.14 7 7h-2z" />
         </svg>
       </div>
+      <CustomColorInput 
+        background={background}
+        setBackground={setBackground}
+        setForeground={setForeground}
+        setAccent={setAccent}
+      ></CustomColorInput>
       <Card
         background={background}
         foreground={foreground}
